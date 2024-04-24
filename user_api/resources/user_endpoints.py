90777bbaead9fa914e2b61394e396027fbe78b77
David Frederick <j.david.frederick.57@gmail.com>
2024-04-23 21:49:00 -0400

    Add basic API calls for listng, creating, and updating users

---
+++ user_api/resources/user_endpoints.py
@@ -0,0 +1,47 @@
+from flask.views import MethodView
+from flask import request
+from flask_smorest import Blueprint, abort
+from db import users
+from schemas import UserSchema, UserUpdateSchema
+
+UserBlueprint = Blueprint('users', __name__, description='REST API operations on users.')
+
+
+@UserBlueprint.route('/users/<string:username>')
+class UserEndpoints(MethodView):
+	@UserBlueprint.response(200, UserSchema)
+	def get(self, username: str):
+		try:
+			return users[username]
+		except KeyError:
+			abort(404, message='User not found')
+
+	@UserBlueprint.arguments(UserUpdateSchema)
+	@UserBlueprint.response(200, UserSchema)
+	def put(self, user_data: UserUpdateSchema, username: str):
+		try:
+			user = users[username]
+			user |= user_data
+			return user
+		except KeyError:
+			abort(404, message='User not found')
+
+
+@UserBlueprint.route('/users')
+class UserListEndpoints(MethodView):
+	@UserBlueprint.response(200, UserSchema(many=True))
+	def get(self):
+		try:
+			sort = request.args.get('order_by') or 'username'
+			if sort in ('username', 'email', 'first_name', 'last_name'):
+				return sorted(users.values(), key=lambda x: x[sort])
+			abort(404, message='Unknown parameter submitted. Please use `order_by` to sort your data')
+		except AttributeError:
+			abort(404, message='Users not accessible')
+
+	# create
+	@UserBlueprint.arguments(UserSchema)
+	@UserBlueprint.response(200, UserSchema)
+	def post(self, user_data: UserSchema):
+		users[user_data['username']] = user_data
+		return user_data, 201
--- user_api/schemas.py
+++ user_api/schemas.py
@@ -3,6 +3,11 @@ from marshmallow import Schema, fields
 
 class UserSchema(Schema):
 	username = fields.Str(required=True)
+	first_name = fields.Str(required=True)
+	last_name = fields.Str(required=True)
+	email = fields.Str(required=True)
+
+class UserUpdateSchema(Schema):
 	first_name = fields.Str()
 	last_name = fields.Str()
 	email = fields.Str()
